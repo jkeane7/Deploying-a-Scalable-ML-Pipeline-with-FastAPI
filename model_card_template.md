38a33285bf10fa92586efd3afbf9134b0270df29
jkeane7 <jkeane7@wgu.edu>
2024-09-29 19:30:21 -0400

    Unit tests passed

--- model_card_template.md
+++ model_card_template.md
@@ -6,19 +6,19 @@ For additional information see the Model Card paper: https://arxiv.org/pdf/1810.
 Model created by Jennifer Keane. It uses RandomForestClassifier which combines multiple decision trees to improve predictive performance and control overfitting. 
 
 ## Intended Use
-Intended use is to develop a classification model on publicly available Census Bureau data.
+Intended use is to develop a classification model on publicly available Census Bureau data and do predictions based on salary of greater than or less than/equal to 50,000.
 
 ## Training Data
 The model will be trained using census data to monitor the model performance on various data slices and will be deployed using the FastAPI package.
 
 ## Evaluation Data
-
+The data included the following parameters: age, workclass, fnlgt,	education, education-num, marital-status, occupation,	relationship, race,	sex, capital-gain, capital-loss, hours-per-week, native-country, salary
 
 ## Metrics
 Model was evaluated using F1 score.  The value is 0.6863
 
 ## Ethical Considerations
-
+We are using sex and race data in the model which may cause some descrimination issues.  Might consider removing from model in the future.
 
 ## Caveats and Recommendations
-No caveats found for this model
+The F1 result was not optimal.  Possibly consider using different classifier or use hyperparameter tuning.
--- test_ml.py
+++ test_ml.py
@@ -1,30 +1,52 @@
 import pytest
 # TODO: add necessary import
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
 
 # TODO: implement the first test. Change the function name and input as needed
-def check_age_restriction(data):
+@pytest.fixture   
+def data():
+    # Sample DataFrame for testing
+    return pd.DataFrame({
+    'age': [16, 17, 18, 22, 20]  # Example ages
+    })
+
+def test_check_age_restriction(data):
     """
-    #  Check that no one under the age of 16 is in the census data.
+    Check that no one under the age of 16 is in the census data.
     """
-    # Your code here
-         
-    return all(data['age'] >= 16)
+    assert all(data['age'] >= 16), "There are individuals under the age of 16 in the census data."
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_dtat_shape():
+def test_data_shape():
     """
-    # check for null values in data
+    check for null values in data
     """
-    # Your code here
+    #Your code here
+    data = pd.DataFrame({
+        'column1': [1, 2, 3],
+        'column2': [4, 5, 6]
+    })
     assert data.shape==data.dropna().shape
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+
+def check_model_algorithm(model, expected_algorithm):
     """
-    # add description for the third test
+    Test to see if model uses correct algorithm, RandomForestClassifier.
     """
-    # Your code here
-    pass
+    model_class_name = model.__class__.__name__
+    return model_class_name == expected_algorithm
+
+def test_check_model_algorithm():
+    """
+    Test to see if model uses the correct algorithm, RandomForestClassifier.
+    """
+    model = RandomForestClassifier()  # Create an instance of the model
+    expected_algorithm = 'RandomForestClassifier'
+    assert check_model_algorithm(model, expected_algorithm) is True
+
+
